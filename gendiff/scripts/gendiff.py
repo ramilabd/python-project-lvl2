44f5c55972ace8bc221768a949b5adc777ff49e1
ramilabd <ramsmart@mail.ru>
2021-03-14 17:51:55 +0500

    added script 'gendiff.py'

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+
+
+"""Gendiff script module."""
+
+
+import argparse
+
+
+def main():
+    """
+    Program start.
+
+    Parameters are missing.
+
+    Returns: None
+    """
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', type=str, help='Path to the first file')
+    parser.add_argument('second_file', type=str, help='Path to the second file')
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == '__main__':
+    main()
