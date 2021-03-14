4fd59bde57790bada4d9837577e66f00e7ac30ab
ramilabd <ramsmart@mail.ru>
2021-03-14 23:18:29 +0500

    added an optional argument "-f, --format"

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -18,6 +18,7 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str, help='Path to the first file')
     parser.add_argument('second_file', type=str, help='Path to the second file')
+    parser.add_argument('-f', '--format', type=str, help='set format of output')
     args = parser.parse_args()
     print(args)
 
