8aada54735a9a71cf5a4e0b08f1db3a83e2962dc
Ramil <ramsmart@mail.ru>
2026-08-01 11:34:23 +0500

    Step 4: read and parse json files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,19 @@
+import json
+
 from gendiff.cli import run
 
 
 def main():
-    run()
+    args = run()
+
+    with open(args.first_file) as file1:
+        data1 = json.load(file1)
+
+    with open(args.second_file) as file2:
+        data2 = json.load(file2)
+
+    print(data1)
+    print(data2)
 
 
 if __name__ == "__main__":
